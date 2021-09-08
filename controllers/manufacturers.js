21ca8830bdfc620186f0cf927820d9356874f7bf
Sherron <sherron@Sherrons-MacBook-Air.local>
2021-09-08 19:49:31 -0400

    added the fuzzy match

--- controllers/manufacturers.js
+++ controllers/manufacturers.js
@@ -1,4 +1,5 @@
 const models = require('../models')
+const { Op } = require('Sequelize')
 
 const getAllManufacturersWithProducts = async (request, response) => {
   const manufacturers = await models.Manufacturers.findAll({
@@ -9,11 +10,13 @@ const getAllManufacturersWithProducts = async (request, response) => {
 }
 
 const getManufacturerById = async (request, response) => {
-  const { id } = request.params
+  const { name } = request.params
 
   const manufacturer = await models.Manufacturers.findOne({
-    where: { id },
-    include: [{ model: models.Products }]
+    where: { name: { [Op.like]: `%${name}%` } },
+    //where: { name: { [Op.substring]: `${name}` } },//
+    include: [{ model: models.Products, attributes: ['id', 'name', 'yearIntroduced'] }],
+    attributes: ['id', 'name', 'country']
   })
 
   return manufacturer
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 /* eslint-disable no-console */
 const express = require('express')
 const { getAllManufacturersWithProducts, getManufacturerById } = require('./controllers/manufacturers')
+const { getAllProducts, getProductsById } = require('./controllers/products')
 
 const app = express()
 
 app.get('/manufacturers', getAllManufacturersWithProducts)
 
-app.get('/manufacturers/:id', getManufacturerById)
+app.get('/manufacturers/:name', getManufacturerById)
+
+app.get('/products', getAllProducts)
+
+app.get('/products/:name', getProductsById)
 
 app.listen(1337, () => {
   console.log('coding rocks!')
