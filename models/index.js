e620b268eb85e279b4373674d00c1c6fd349bcc4
Sherron <sherron@Sherrons-MacBook-Air.local>
2021-09-08 20:07:28 -0400

    changed the configs in index.js

--- models/index.js
+++ models/index.js
@@ -1,9 +1,14 @@
 const Sequelize = require('sequelize')
 const ManufacturersModel = require('./manufacturers.js')
 const ProductsModel = require('./products.js')
+const AllConfigs = require('../config/sequelize')
+const environment = process.env.NODE_ENV || 'development'
+const {
+  username, password, database, host, dialect
+} = AllConfigs[environment]
 
-const connection = new Sequelize('candies', 'candies', 'C4nd13$!', {
-  host: 'localhost', dialect: 'mysql'
+const connection = new Sequelize(database, username, password, {
+  host, dialect
 })
 
 const Manufacturers = ManufacturersModel(connection, Sequelize)
